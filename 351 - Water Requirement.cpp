63fa4c440ad14d85fe5c5641651ddf2009eee864
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-05-01 18:57:45 +0600

    Create 351 - Water Requirement.cpp

---
+++ 351 - Water Requirement.cpp
@@ -0,0 +1,21 @@
+// Finally, after purchasing a water cooler during the April long challenge, Chef noticed that his water cooler requires 
+// 2
+// 2 liters of water to cool for one hour.
+
+// How much water (in liters) would be required by the cooler to cool for 
+// N
+// N hours?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int count,x;
+	cin>>count;
+	for (int i = 0; i < count; i++) {
+	    cin>>x;
+	    std::cout << (x*2) << std::endl;
+	}
+
+}
