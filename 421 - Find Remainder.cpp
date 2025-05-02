3d2a1e6303ccd8f71363c63e9685b70fdce1754f
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-05-02 20:50:22 +0600

    Create 421 - Find Remainder.cpp

---
+++ 421 - Find Remainder.cpp
@@ -0,0 +1,15 @@
+// Write a program to find the remainder when an integer A is divided by an integer B.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int count,x,y;
+	cin>>count;
+	for (int i = 0; i < count; i++) {
+	    cin>>x >>y;
+	    std::cout << x%y << std::endl;
+	}
+
+}
