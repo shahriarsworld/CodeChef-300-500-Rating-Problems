5c78b674dcc054dd85b6eeeae6d0dbe83936094a
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-05-02 17:09:44 +0600

    Create 405 - Height of Rationals.cpp

---
+++ 405 - Height of Rationals.cpp
@@ -0,0 +1,50 @@
+// In a recent breakthrough in mathematics, the proof utilized a concept called Height.
+
+// Consider a fraction 
+// a
+// b
+// b
+// a
+// ​
+//  . Its Height is defined as the maximum of its numerator and denominator. So, for example, the Height of 
+// 3
+// 19
+// 19
+// 3
+// ​
+//   would be 
+// 19
+// 19, and the Height of 
+// 27
+// 4
+// 4
+// 27
+// ​
+//   would be 
+// 27
+// 27.
+
+// Given 
+// a
+// a and 
+// b
+// b, find the Height of 
+// a
+// b
+// b
+// a
+// ​
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x >>y;
+	if(x>y){
+	    std::cout << x << std::endl;
+	}else{
+	    std::cout << y << std::endl;
+	}
+
+}
