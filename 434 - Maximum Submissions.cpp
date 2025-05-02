84496f70108d7d9604da06e5f18e40981ce6f1ac
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-05-03 01:21:18 +0600

    Create 434 - Maximum Submissions.cpp

---
+++ 434 - Maximum Submissions.cpp
@@ -0,0 +1,25 @@
+// A participant can make 
+// 1
+// 1 submission every 
+// 30
+// 30 seconds. If a contest lasts for 
+// X
+// X minutes, what is the maximum number of submissions that the participant can make during it?
+
+// It is also given that the participant cannot make any submission in the last 
+// 5
+// 5 seconds of the contest.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int count,x;
+	cin>>count;
+	for (int i = 0; i < count; i++) {
+	    cin>>x;
+	    std::cout << x/0.5 << std::endl;
+	}
+
+}
